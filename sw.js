181301ea893753247551a34f2e03f652202ac411
Faav <faavemc@gmail.com>
2026-01-21 17:11:25 -0500

    Update version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'totp.codes-v1.1';
+const CACHE_NAME = 'totp.codes-v1.2';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting(); // Make this SW activate immediately
